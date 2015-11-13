3bc1056bfd2f468dbded37a59593b5329aaf683f
Andrew <andrewshirokoff@gmail.com>
2015-11-13 19:26:51 +0500

    add base code for optimization tasks

---
+++ Chapter 5/optimization.py
@@ -0,0 +1,72 @@
+__author__ = 'Andrew'
+import time
+import random
+import math
+
+working_path = "C:\\Users\\Andrew\\Source\\Repos\\CollIntel\\Chapter 5\\"
+
+people = [('Seymour','LED'),
+          ('Franny','AER'),
+          ('Zooey','SVX'),
+          ('Walt','ROV')]
+
+destination='DME'
+
+def getminutes(t):
+    x=time.strptime(t,'%H:%M')
+    return x[3]*60+x[4]
+
+def printschedule(r):
+    for d in range(len(r)/2):
+        name=people[d][0]
+        origin=people[d][1]
+        out=flights[(origin,destination)][r[d]]
+        ret=flights[(destination,origin)][r[d+1]]
+        print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,
+                                                      out[0],out[1],out[2],
+                                                      ret[0],ret[1],ret[2])
+
+def schedulecost(sol):
+    totalprice=0
+    latestarrival=0
+    earliestdep=24*60
+
+    for d in range(len(sol)/2):
+        # Get the inbound and outbound flights
+        origin=people[d][1]
+        outbound=flights[(origin,destination)][int(sol[d])]
+        returnf=flights[(destination,origin)][int(sol[d+1])]
+
+        # Total price is the price of all outbound and return flights
+        totalprice+=outbound[2]
+        totalprice+=returnf[2]
+
+        # Track the latest arrival and earliest departure
+        if latestarrival<getminutes(outbound[1]): latestarrival=getminutes(outbound[1])
+        if earliestdep>getminutes(returnf[0]): earliestdep=getminutes(returnf[0])
+
+    # Every person must wait at the airport until the latest person arrives.
+    # They also must arrive at the same time and wait for their flights.
+    totalwait=0
+    for d in range(len(sol)/2):
+        origin=people[d][1]
+        outbound=flights[(origin,destination)][int(sol[d])]
+        returnf=flights[(destination,origin)][int(sol[d+1])]
+        totalwait+=latestarrival-getminutes(outbound[1])
+        totalwait+=getminutes(returnf[0])-earliestdep
+
+    # Does this solution require an extra day of car rental? That'll be $50!
+    if latestarrival>earliestdep: totalprice+=50
+    return totalprice+totalwait
+
+flights = {}
+for line in file(working_path + 'schedule.txt'):
+    origin,dest,depart,arrive,price=line.strip().split(',')
+    flights.setdefault((origin,dest),[])
+
+    # Add details to the list of possible flights
+    flights[(origin,dest)].append((depart,arrive,int(price)))
+
+s = [1 for x in xrange(8)]
+printschedule(s)
+print schedulecost(s)
