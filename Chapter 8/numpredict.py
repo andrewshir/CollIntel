b20824cae35daddca9a4526e320dcf821d055cc7
Andrew <andrewshirokoff@gmail.com>
2016-01-26 11:57:56 +0500

    add KNN functions with weights

---
+++ Chapter 8/numpredict.py
@@ -0,0 +1,176 @@
+from random import random,randint
+import math
+import optimization
+
+def wineprice(rating,age):
+    peak_age=rating-50
+    # Calculate price based on rating
+    price=rating/2
+    if age>peak_age:
+        # Past its peak, goes bad in 5 years
+        price=price*(5-(age-peak_age))
+    else:
+        # Increases to 5x original value as it
+        # approaches its peak
+        price=price*(5*((age+1)/peak_age))
+    if price<0: price=0
+    return price
+
+
+def wineset1():
+    rows = []
+    for i in range(300):
+        # Create a random age and rating
+        rating = random()*50+50
+        age = random()*50
+        # Get reference price
+        price = wineprice(rating, age)
+        # Add some noise
+        price *= (random()*0.4+0.8)
+        # Add to the dataset
+        rows.append({'input': (rating, age), 'result':price})
+    return rows
+
+
+def wineset2():
+    rows = []
+    for i in range(300):
+        # Create a random age and rating
+        rating = random()*50+50
+        age = random()*50
+        aisle=float(randint(1,20))
+        bottlesize=[375.0,750.0,1500.0,3000.0][randint(0,3)]
+        # Get reference price
+        price = wineprice(rating, age)
+        price*=(bottlesize/750)
+        # Add some noise
+        price *= (random()*0.4+0.8)
+        # Add to the dataset
+        rows.append({'input' : (rating, age, aisle, bottlesize), 'result':price})
+    return rows
+
+
+def euclidean(v1, v2):
+    sum = 0.0
+    for i in xrange(len(v1)):
+        sum += (v1[i] - v2[i]) ** 2
+    return math.sqrt(sum)
+
+
+def getdistances(data, vec1):
+    distancelist = []
+    for i in range(len(data)):
+        vec2 = data[i]['input']
+        distancelist.append((euclidean(vec1, vec2), i))
+    distancelist.sort()
+    return distancelist
+
+
+def knnestimate(data, vec1, k=3):
+    dlist=getdistances(data, vec1)
+    avg = 0.0
+
+    for i in xrange(k):
+        idx = dlist[i][1]
+        avg += data[idx]['result']
+
+    avg = avg/k
+    return avg
+
+
+def inverseweight(dist, num=1.0, const=0.1):
+    return num/(dist+const)
+
+
+def subtractweight(dist, const=1.0):
+    if dist > const:
+        return 0
+    else:
+        return const-dist
+
+
+def gaussian(dist, sigma=10.0):
+    return math.e**(-dist**2/(2*sigma**2))
+
+
+def weightknn(data, vec1, k=3, weightf=gaussian):
+    dlist=getdistances(data, vec1)
+    avg = 0.0
+    totalweight = 0.0
+
+    for i in xrange(k):
+        dist = dlist[i][0]
+        idx = dlist[i][1]
+        weight = weightf(dist)
+        avg += weight*data[idx]['result']
+        totalweight += weight
+
+    avg = avg/totalweight
+    return avg
+
+
+def dividedata(data,test=0.05):
+    trainset = []
+    testset = []
+    for row in data:
+        if random() < test:
+            testset.append(row)
+        else:
+            trainset.append(row)
+    return trainset, testset
+
+
+def testalgorithm(algf, trainset, testset):
+    error = 0.0
+    for row in testset:
+        guess = algf(trainset, row['input'])
+        error += (row['result']-guess)**2
+    return error/len(testset)
+
+
+def crossvalidate(algf, data, trials=100, test=0.05):
+    error = 0.0
+    for i in range(trials):
+        trainset, testset = dividedata(data, test)
+        error += testalgorithm(algf, trainset, testset)
+    return error/trials
+
+
+def rescale(data, scale):
+    scaleddata = []
+    for row in data:
+        scaled = [scale[i]*row['input'][i] for i in range(len(scale))]
+        scaleddata.append({'input': scaled, 'result': row['result']})
+    return scaleddata
+
+def createcostfunction(algf, data):
+    def costf(scale):
+        sdata=rescale(data, scale)
+        return crossvalidate(algf, sdata, trials=10)
+    return costf
+
+# data = wineset1()
+# print data[0]
+# print data[1]
+# print wineprice(95.0, 3.0)
+
+
+def knn1(d, v):
+    return knnestimate(d, v, k=1)
+
+
+def knn3(d, v):
+    return knnestimate(d, v, k=3)
+
+data = wineset2()
+
+# scales = [8, 1, 18, 16]
+
+costf = createcostfunction(knnestimate, data)
+weightdomain = [20] * 4
+scales, value = optimization.simulated_annealing_optimization(weightdomain, costf)
+
+print scales
+sdata = rescale(data, scales)
+print crossvalidate(knn3, sdata)
+
---
+++ Chapter 8/optimization.py
@@ -0,0 +1,212 @@
+__author__ = 'Andrew'
+import time
+import sys
+import random
+import math
+
+working_path = "C:\\Users\\Andrew\\Source\\Repos\\CollIntel\\Chapter 5\\"
+
+people = [('Seymour','LED'),
+          ('Franny','AER'),
+          ('Zooey','SVX'),
+          ('Walt','ROV')]
+
+destination='DME'
+
+
+
+def getminutes(t):
+    x=time.strptime(t,'%H:%M')
+    return x[3]*60+x[4]
+
+def printschedule(r):
+    for d in range(len(r)/2):
+        name=people[d][0]
+        origin=people[d][1]
+        out=flights[(origin,destination)][r[d*2]]
+        ret=flights[(destination,origin)][r[d*2+1]]
+        print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,
+                                                      out[0],out[1],out[2],
+                                                      ret[0],ret[1],ret[2])
+
+def schedulecost(sol):
+    totalprice=0
+    latestarrival=0
+    earliestdep=24*60
+
+    for d in range(len(sol)/2):
+        # Get the inbound and outbound flights
+        origin=people[d][1]
+        outbound=flights[(origin,destination)][int(sol[d*2])]
+        returnf=flights[(destination,origin)][int(sol[d*2+1])]
+
+        # Total price is the price of all outbound and return flights
+        totalprice+=outbound[2]
+        totalprice+=returnf[2]
+
+        # Track the latest arrival and earliest departure
+        if latestarrival<getminutes(outbound[1]): latestarrival=getminutes(outbound[1])
+        if earliestdep>getminutes(returnf[0]): earliestdep=getminutes(returnf[0])
+
+    # Every person must wait at the airport until the latest person arrives.
+    # They also must arrive at the same time and wait for their flights.
+    totalwait=0
+    for d in range(len(sol)/2):
+        origin=people[d][1]
+        outbound=flights[(origin,destination)][int(sol[d*2])]
+        returnf=flights[(destination,origin)][int(sol[d*2+1])]
+        totalwait+=latestarrival-getminutes(outbound[1])
+        totalwait+=getminutes(returnf[0])-earliestdep
+
+    # Does this solution require an extra day of car rental? That'll be $50!
+    if latestarrival>earliestdep: totalprice+=50
+    return totalprice+totalwait
+
+def random_optimization(domain, cost, iter=1000):
+    best = sys.maxint
+    result = None
+
+    for j in xrange(iter):
+        s = []
+        for i in xrange(len(domain)):
+            s.append(random.randint(0, domain[i])-1)
+
+        c = cost(s)
+        if c < best:
+            best = c
+            result = s
+    return result, best
+
+def hill_climb_optimization(domain, cost):
+    best = sys.maxint
+    result = None
+
+    # define random initial solution
+    s = []
+    for i in xrange(len(domain)):
+        s.append(random.randint(0, domain[i])-1)
+    best_n = cost(s)
+
+    while best_n < best:
+        best = best_n
+        result = s
+        best_n = sys.maxint
+        neighbors = []
+        for i in xrange(len(s)):
+            if s[i] < domain[i]-1:
+                n = []
+                n.extend(s)
+                n[i] = n[i] + 1
+                neighbors.append(n)
+            if s[i] > 0:
+                n = []
+                n.extend(s)
+                n[i] = n[i] - 1
+                neighbors.append(n)
+
+        for neighbor in neighbors:
+            c = cost(neighbor)
+            if c < best_n:
+                best_n = c
+                s = neighbor
+    return result, best
+
+def generic_simulated_annealing_optimization(domain, costf):
+    best = sys.maxint
+    result = None
+    for i in xrange(15):
+        sol, cost = simulated_annealing_optimization(domain, costf)
+        if cost < best:
+            result = sol
+            best = cost
+    return result, best
+
+def simulated_annealing_optimization(domain, costf, T=10000, cooldown=0.95):
+    #initial solution
+    sol = [random.randint(0, x-1) for x in domain]
+    cost = costf(sol)
+
+    while T > 1.0:
+        nsol = []
+        nsol.extend(sol)
+        c = random.randint(0, len(domain)-1)
+        if random.random() < 0.5 and sol[c] < domain[c]-1:
+            nsol[c] = sol[c] + 1
+        elif sol[c] > 0:
+            nsol[c] = sol[c] - 1
+
+        ncost = costf(nsol)
+        p = pow(math.e, (-ncost-cost)/float(T))
+
+        if random.random() < p or ncost < cost:
+            sol = nsol
+            cost = ncost
+
+        T *= cooldown
+    return sol, cost
+
+def genetic_optimization(domain, costf, popsize=50, elite=0.2, mutprob=0.3, not_changed_cost_count=50):
+    def mutate(sol):
+        result = []
+        result.extend(sol)
+        c = random.randint(0, len(domain)-1)
+        if random.random() < 0.5 and sol[c] > 0:
+            result[c] -= 1
+        elif sol[c] < domain[c]-1:
+            result[c] += 1
+        return result
+
+    def crossover(sol1, sol2):
+        result = []
+        c = random.randint(1, len(domain)-2)
+        result = sol1[0:c] + sol2[c:]
+        return result
+
+    def all_the_same(list):
+        if len(list) < 2:
+            return False
+        for el in list:
+            if el != list[0]:
+                return False
+        return True
+
+    # initial population
+    pop = []
+    for i in xrange(50):
+        pop.append([random.randint(0, x-1) for x in domain])
+
+    latest_sol = []
+
+    while not all_the_same(latest_sol):
+        # define best solutions
+        scored_pop = [(costf(s), s) for s in pop]
+        scored_pop.sort(key=lambda tup: tup[0])
+        topsol = int(elite*len(scored_pop))
+
+        pop = []
+        pop.extend([tup[1] for tup in scored_pop[0:topsol]])
+        while len(pop) < popsize:
+            if random.random() < mutprob:
+                # mutation
+                c = random.randint(0, topsol-1)
+                pop.append(mutate(pop[c]))
+            else:
+                # crossover
+                c1 = random.randint(0, topsol-1)
+                c2 = random.randint(0, topsol-1)
+
+                # to be sure we don't cross the same solution
+                while c1 == c2:
+                    c1 = random.randint(0, topsol-1)
+                    c2 = random.randint(0, topsol-1)
+
+                pop.append(crossover(pop[c1], pop[c2]))
+
+        latest_sol.append(scored_pop[0][0])
+        if len(latest_sol) > not_changed_cost_count:
+            latest_sol.remove(latest_sol[0])
+        print scored_pop[0][0]
+
+    return scored_pop[0][1], scored_pop[0][0]
+
+
